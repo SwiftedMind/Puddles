e7e4dc770c86053f92c68f015e982e3dc6d5623a
Dennis Müller <d3mueller@me.com>
2023-01-04 19:05:24 +0100

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
+			.package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     ],
     targets: [
         .target(
