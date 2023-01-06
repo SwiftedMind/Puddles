8b9541b04afa7dfdf087debfeb8dcff47b93116c
Dennis Müller <d3mueller@me.com>
2023-01-06 09:55:16 +0100

    Added example project

---
+++ Examples/PuddlesExampleiOS/PuddlesExampleiOS/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  PuddlesExampleIOS
+//
+//  Created by Dennis Müller on 06.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
