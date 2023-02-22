00d1c9892e019556692d6ac9c4f035b4d7c32c56
Dennis Müller <d3mueller@me.com>
2023-02-22 20:49:21 +0100

    Added signals

--- Sources/Puddles/TabViewNavigator/Signal.swift
+++ Sources/Puddles/TabViewNavigator/Signal.swift
@@ -1,3 +1,39 @@
 import SwiftUI
 
+@propertyWrapper
+public struct Signal<Content>: DynamicProperty {
+    @State var content: Content? = nil
+    @State var id: UUID = .init()
 
+    public var wrappedValue: Wrapped {
+        .init(id: id, content: content) { content in
+            send(content)
+        }
+    }
+
+    public init() {
+
+    }
+
+    private func send(_ content: Content) {
+        self.content = content
+        id = .init()
+    }
+
+}
+
+public extension Signal {
+    struct Wrapped: Equatable {
+        var id: UUID
+        public var content: Content?
+        var onSend: (_ content: Content) -> Void
+
+        public func send(_ content: Content) {
+            onSend(content)
+        }
+
+        public static func == (lhs: Signal<Content>.Wrapped, rhs: Signal<Content>.Wrapped) -> Bool {
+            lhs.id == rhs.id
+        }
+    }
+}
