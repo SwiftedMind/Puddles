b0f404c02c575c04ed28b2e3ac9449fcfa8bb414
Dennis Müller <d3mueller@me.com>
2023-06-28 13:34:12 +0200

    initial

---
+++ Sources/Puddles/WithAdapter.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dennis Müller on 08.06.23.
+//
+
+import Foundation
+
+//@freestanding(declaration)
+//macro registerProvider<Provider: AnyObject>() {
+//    return "ABC"
+//}
